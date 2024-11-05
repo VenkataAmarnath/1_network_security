d8d9d54ef4902fc4c1eee9817028ed1d2fba51a9
amar <pendelaamarnath@gmail.com>
2024-11-05 17:07:47 +0530

    constant, config,crtifact structure updated

---
+++ network_security/entity/config_entity.py
@@ -0,0 +1,27 @@
+import os
+from network_security.constants import training_pipeline
+from datetime import datetime
+
+print(training_pipeline.ARTIFACT_DIR)
+
+class TrainingPipelineConfig:
+    pass
+
+class DataIngestionConfig:
+    pass
+
+
+class DataValidationConfig:
+    pass
+
+class DataTransformationConfig:
+    pass
+
+class ModelTrainerConfig:
+    pass
+
+class ModelEvaluationConfig:
+    pass
+
+class ModelPusherConfig:
+    pass
