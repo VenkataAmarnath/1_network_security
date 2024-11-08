956f7ea75f9be6551cf5e65e4c75befe1f240f92
amar <pendelaamarnath@gmail.com>
2024-11-08 13:45:07 +0530

    Data Validation: updated utils to write report.yaml file in artifacts

--- network_security/utils/main_utils/utils.py
+++ network_security/utils/main_utils/utils.py
@@ -16,12 +16,13 @@ def read_yaml_file(file_path: str):
 
 def write_yaml_file(file_path: str, content: object, replace: bool=False)-> None:
     try:
+        #print(file_path)
         if replace:
             if os.path.exists(file_path):
                 os.remove(file_path)
-            os.makedirs(os.path.dirname(file_path))
-            with open(file_path,'w') as file:
-                yaml.dump(content,file)
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        with open(file_path,'w') as file:
+            yaml.dump(content,file)
 
 
 
